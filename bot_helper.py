45609fb50b8803ed52f1df4fed07673dc81cd665
lana-sviatash <128919364+lana-sviatash@users.noreply.github.com>
2023-07-08 20:17:00 +0300

    Code in OOP

--- bot_helper.py
+++ bot_helper.py
@@ -1,123 +1,247 @@
 import difflib
 import functools
 import os
-
+from contact_manager import *
 from accessary import welcome_message, hello_instruction, parser_input
 
 
-contacts_dict = {}
+class Bot:
+    def __init__(self):
+        self.address_book = AddressBook()
 
-def load_contacts_from_file():
-    if os.path.exists('contacts.txt'):
-        with open('contacts.txt', 'r') as file:
-            for line in file:
-                line = line.strip()
-                if line:
-                    name, phone = line.split(':')
-                    contacts_dict[name.strip()] = phone.strip()
-
-def input_errors(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except (TypeError, KeyError, ValueError, IndexError) as e:
-            error_message = str(e).split(':')[1]
-            return f"Give me {error_message}"
-
-    return wrapper
-
-@input_errors
-def adding_contact(name: str, phone: str):
-    if phone.isnumeric() and len(phone) in range(10, 13):
-        contacts_dict[name.capitalize()] = phone
-        save_contacts_to_file()
-        return 'Contact added successfully'
-    else:
-        return 'Phone number should have 10-12 numbers without space'
-
-
-@input_errors
-def changing_contact(name: str, phone: str):
-    if phone.isnumeric() and len(phone) in range(10, 13):
-        contacts_dict[name.capitalize()] = phone
-        save_contacts_to_file()
-        return 'Contact changed successfully'
-    else:
-        return 'Phone number should have 10-12 numbers without space'
-
-def save_contacts_to_file():
-    with open("contacts.txt", "w") as file:
-        for name, phone in contacts_dict.items():
-            file.write(f"{name}: {phone}\n")
-
-@input_errors
-def phone(name: str):
-    if contacts_dict[name.capitalize()]:
-        return contacts_dict[name.capitalize()]
-    else:
-        return 'Contact not found'
-
-def show_contacts(*args):
-    if not contacts_dict:
-        return 'No contacts'
-    
-    contacts = ""
-    for name, phone in contacts_dict.items():
-        contact = f"{name}: {phone}"
-        contacts += contact + "\n"
-    return contacts.rstrip('\n')
-
-@input_errors
-def delete_contact(name: str):
-    if name.capitalize() in contacts_dict:
-        del contacts_dict[name.capitalize()]
-        save_contacts_to_file()  # Save the updated contacts to file
-        return f"Contact '{name.capitalize()}' deleted successfully."
-    else:
-        return f"Contact '{name.capitalize()}' not found."
-
-command_dict = {
-    'add': [adding_contact, 'add contact'],
-    'change': [changing_contact, 'change existing phone of the contact'],
-    'phone': [phone, 'show phone of existing contact'],
-    'show all': [show_contacts, 'show all contacts'],
-    'del': [delete_contact, 'delete the contact']
-}
-
-def command_handler(user_input, command_dict):
-    if user_input in command_dict:
-        return command_dict[user_input][0]
-    possible_command = difflib.get_close_matches(user_input.split()[0], command_dict, cutoff=0.6)
-    if possible_command:
-        return f'An unknown command. Maybe you mean: {", ".join(possible_command)}'
-    else:
-        return f'An unknown command.'
-
-def main():
-    print(welcome_message())
-
-    if "contacts.txt":
-        load_contacts_from_file()
-
-    while True:
-        user_input = input('>>> ').lower()
-        if user_input == 'hello':
-            print("How can I help you?\n")
-            print(hello_instruction(command_dict))
-        elif user_input in ('good bye', "close", "exit"):
-            if os.path.exists("contacts.txt") and os.path.getsize("contacts.txt") == 0:
-                os.remove("contacts.txt")
-            print('Good bye!')
-            break
+    def load_contacts_from_file(self):
+        if os.path.exists('contacts.txt'):
+            with open('contacts.txt', 'r') as file:
+                for line in file:
+                    line = line.strip()
+                    if line:
+                        name, phones = line.split(':')
+                        record = Record(Name(name.strip()))
+                        for phone in phones.strip().split(','):
+                            phone = Phone(phone.strip())
+                            record.add_phone(phone)
+                        self.address_book.add_record(record)
         else:
-            command, arguments = parser_input(user_input, command_dict)
-            if command in command_dict:
-                result = command_handler(command, command_dict)(*arguments)
-            else:
-                result = command_handler(user_input, command_dict)
-            print(result)
+            return self.address_book
         
+    def save_contacts_to_file(self):
+        with open("contacts.txt", "w") as file:
+            for name, record in self.address_book.data.items():
+                phones = ", ".join(str(phone) for phone in record.phones)
+                file.write(f"{name}: {phones}\n")
+    
+    def input_errors(func):
+        @functools.wraps(func)
+        def wrapper(self, *args, **kwargs):
+            try:
+                return func(self, *args, **kwargs)
+            except (TypeError, KeyError, ValueError, IndexError) as e:
+                error_message = str(e).split(':')[1]
+                return f"Give me {error_message}"
+        return wrapper
+
+    
+    @input_errors
+    def adding_contact(self, name:str, phone=None):
+        try:
+            name = Name(name)
+            name.check_name(name.value)
+        except NameError as e:
+            return e
+        
+        if phone:
+            phone = Phone(phone)
+            try:
+                phone.check_phone(phone.value)
+            except PhoneError as e:
+                return e
+            record = Record(name.check_name(name.value))
+            record.add_phone(phone.check_phone(phone.value))
+        else:
+        
+        # record = Record(name.check_name(name.value), phone)
+            record = Record(name.check_name(name.value))
+        self.address_book.data[record.name] = record
+        self.save_contacts_to_file()
+        return 'Contact added successfully'
+    
+    @input_errors
+    def adding_contact_phone(self, name: str, phone: str):
+        name = Name(name.capitalize())
+        try:
+            name.check_name(name.value)
+        except NameError as e:
+            return e
+
+        record = self.address_book.data.get(name.value)
+        if record:
+            try:
+                phone = Phone(phone)
+                phone.check_phone(phone.value)
+            except PhoneError as e:
+                return e
+            
+            existing_phones = [str(p)[-10:] for p in record.phones]
+            new_phone = phone.check_phone(phone.value)[-10:]
+            if new_phone in existing_phones:
+                return f"The phone number {phone.value} already exists in the contact."
+        
+            record.add_phone(phone.check_phone(phone.value))
+            self.save_contacts_to_file()
+            return 'Phone added successfully'
+        else:
+            return f"No record found for {name.value}"
+
+    @input_errors
+    def changing_contact_name(self, old_name: str, new_name: str):
+        old_name = Name(old_name.capitalize())
+        try:
+            old_name.check_name(old_name.value)
+        except NameError as e:
+            return e
+
+        new_name = Name(new_name.capitalize())
+        try:
+            new_name.check_name(new_name.value)
+        except NameError as e:
+            return e
+
+        old_record = self.address_book.data.get(old_name.value)
+        if old_record:
+            new_record = Record(new_name, phone=old_record.phones)
+            try:
+                self.address_book.change_record_name(old_record, new_record)
+                self.save_contacts_to_file()
+                return 'Contact changed successfully'
+            except NameError as e:
+                return str(e)
+        else:
+            return f"No record found for {old_name.value}"
+
+    @input_errors
+    def changing_contact_phone(self, name: str, old_phone: str, new_phone: str):
+        name = Name(name.capitalize())
+        record = self.address_book.data.get(name.value)
+
+        if record:
+            try:
+                old_phone = Phone(old_phone)
+                old_phone.check_phone(old_phone.value)
+                new_phone = Phone(new_phone)
+                new_phone.check_phone(new_phone.value)
+            except PhoneError as e:
+                return e
+            if record.change_phone(old_phone.check_phone(old_phone.value), new_phone.check_phone(new_phone.value)):
+                self.save_contacts_to_file()
+                return 'Phone changed successfully'
+            else:
+                return f"Phone number '{old_phone.check_phone(old_phone.value)}' not found in the record."
+        else:
+            return f"No record found for {name.value}"
+
+    @input_errors
+    def show_phone(self, name: str):
+        name = Name(name.capitalize())
+        record = self.address_book.data.get(name.value)
+        if record:
+            if record.phones:
+                return f"The phone(s) for {record.name.value} are: {', '.join(str(phone) for phone in record.phones)}"
+            else:
+                return f"No phone numbers found for {record.name.value}"
+        else:
+            return f"No record found for {name.value}"
+
+    @input_errors
+    def show_contact(self, name: str):
+        name = Name(name.capitalize())
+        record = self.address_book.data.get(name.value)
+
+        try:
+            result = self.address_book.show_record(record)
+            return result
+        except NameError as e:
+            return str(e)
+
+
+    def show_book(self):
+        return self.address_book.show_records()
+        
+    @input_errors
+    def delete_contact(self, name: str):
+        name = Name(name.capitalize())
+        record = Record(name)
+        try:
+            self.address_book.delete_record(record)
+            self.save_contacts_to_file()
+            return 'Contact deleted successfully'
+        except NameError as e:
+            return str(e)
+
+    @input_errors
+    def delete_phone(self, name: str, phone: str):
+        name = Name(name.capitalize())
+        record = self.address_book.data.get(name.value)
+
+        try:
+            phone = Phone(phone)
+            phone.check_phone(phone.value)
+        except PhoneError as e:
+            return e
+
+        if record:
+            phone_to_delete = phone.check_phone(phone.value)
+            if record.del_phone(phone_to_delete):
+                self.save_contacts_to_file()
+                return f'Phone number {phone_to_delete} deleted successfully'
+            else:
+                return f'Phone number {phone_to_delete} not found in the record'
+        else:
+            return f'No record found for {name.value}'
+
+    command_dict = {
+        'add': [adding_contact, 'add contact name'],
+        'phone': [adding_contact_phone, 'add phone to existing contact'],
+        'change name': [changing_contact_name, 'change existing name of the contact'],
+        'change phone': [changing_contact_phone, 'change existing phone of the contact'],
+        'show phone': [show_phone, 'show phone of existing contact'],
+        'show contact': [show_contact, 'show existing contact'],
+        'show book': [show_book, 'show all contacts'],
+        'del contact': [delete_contact, 'delete the contact'],
+        'del phone': [delete_phone, 'delete the phone of contact']
+        }
+    
+    def command_handler(self, user_input, command_dict):
+        if user_input in command_dict:
+            return command_dict[user_input][0]
+        possible_command = difflib.get_close_matches(user_input.split()[0], command_dict, cutoff=0.6)
+        if possible_command:
+            return f'An unknown command. Maybe you mean: {", ".join(possible_command)}'
+        else:
+            return f'An unknown command.'
+    
+    def main(self):
+        print(welcome_message())
+        self.load_contacts_from_file()
+
+        while True:
+            user_input = input('>>> ').lower()
+            if user_input == 'hello':
+                print("How can I help you?\n")
+                print(hello_instruction(self.command_dict))
+            elif user_input in ('good bye', "close", "exit"):
+                if os.path.exists("contacts.txt") and os.path.getsize("contacts.txt") == 0:
+                    os.remove("contacts.txt")
+                print('Good bye!')
+                break
+            else:
+                command, arguments = parser_input(user_input, self.command_dict)
+                if command in self.command_dict:
+                    result = self.command_handler(command, self.command_dict)(self, *arguments)
+                else:
+                    result = self.command_handler(user_input, self.command_dict)
+                print(result)
+    
 
 if __name__ == "__main__":
-    main()
+    bot = Bot()
+    bot.main()
---
+++ contact_manager.py
@@ -0,0 +1,173 @@
+from collections import UserDict
+
+
+class PhoneError(Exception):
+    pass
+
+class NameError(Exception):
+    pass
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+        
+
+
+class Name(Field):
+    def check_name(self, name:str):
+        if len(name) < 2:
+            raise NameError('Name should have more then 1 char')
+        if str(name).startswith(' '):
+            raise NameError('Name should start with a letter')
+        return name.capitalize()
+    
+    def __str__(self):
+        return self.value
+
+
+
+class Phone(Field):
+    def __str__(self):
+        return self.value
+    
+    def check_phone(self, phone):
+        if phone.isnumeric() and len(phone) in range(10, 13):
+            if len(phone) == 10:
+                phone = '+38' + phone
+            elif len(phone) == 11:
+                phone = '+3' + phone
+            elif len (phone) == 12:
+                phone = '+' + phone
+            return phone
+        else:
+            raise PhoneError('Phone number should have 10-12 numbers without space')
+        
+
+class Record:
+    def __init__(self, name, phone:Phone=None):
+        self.name = name
+        self.phones = []
+        if phone:
+            self.phones.append(phone)
+    
+    def __str__(self):
+        phone_numbers = ', '.join(str(phone) for phone in self.phones)
+        return f'{self.name}: {phone_numbers}'
+
+    def add_phone(self, phone:Phone):
+        self.phones.append(phone)
+    
+    def del_phone(self, phone: Phone):
+        phone_str = str(phone)
+        for i, record_phone in enumerate(self.phones):
+            if str(record_phone) == phone_str:
+                self.phones.pop(i)
+                return True
+        return False
+
+    def change_phone(self, old_phone:Phone, new_phone:Phone):
+        old_phone_str = str(old_phone)
+        new_phone_str = str(new_phone)
+        for i, phone in enumerate(self.phones):
+            if str(phone) == old_phone_str:
+                self.phones[i] = new_phone_str
+                return True
+        return False
+
+class AddressBook(UserDict):
+    def add_record(self, record:Record):
+        self.data[record.name.value] = record
+    
+    def delete_record(self, record:Record):
+        if record.name.value in self.data:
+            del self.data[record.name.value]
+        else:
+            raise NameError(f'Record with name {record.name.value} not found in the address book.')
+    
+    def change_record_name(self, old_record:Record, new_record:Record):
+        if old_record.name.value in self.data:
+            self.data[new_record.name.value] = self.data.pop(old_record.name.value)
+        else:
+            raise NameError(f'Record with name {old_record.name.value} not found in the address book.')
+
+    def show_records(self):
+        rows = []
+        for record in self.data.values():
+            phones = [str(phone) for phone in record.phones]
+            row = f'{record.name.value}: {", ".join(phones)}'
+            rows.append(row)
+        return '\n'.join(rows)
+        
+    def show_record(self, record:Record):
+        if record.name.value in self.data:
+            phones = [str(phone) for phone in record.phones]
+            return f'{record.name.value}: {", ".join(phones)}'
+        else:
+            raise NameError(f"Record with name '{record.name.value}' not found in the address book.")
+    
+    def __repr__(self):
+        return "\n".join(str(record) for record in self.data.values())
+
+
+if __name__ == '__main__':
+    ab = AddressBook()
+
+    # Create a Name instance
+    name = Name("bill")
+
+    try:
+        # Check the validity of the name
+        name_value = name.check_name(name.value)
+        print(name_value)  # Print the validated name
+    except NameError as e:
+        print(str(e))
+
+    # Create a Phone instance
+    phone1 = Phone("12345")
+
+    try:
+        # Check the validity of the name
+        name.check_name(name.value)
+    except NameError as e:
+        print(str(e))
+
+    try:
+        # Check the validity of the phone number
+        phone1.check_phone(phone1.value)
+    except PhoneError as e:
+        print(str(e))
+
+    # Create a Record with name and phone number
+    rec = Record(name, phone1)
+    print(rec.name)
+
+    # Add the record to the address book
+    ab.add_record(rec)
+
+    # Add a record without a phone number
+    ab.add_record(Record(Name("Jill")))
+
+    # Check if the records in the address book are instances of Record
+    for rec in ab.values():
+        assert isinstance(rec, Record)
+
+    # Create another Phone instance
+    phone2 = Phone("56784")
+
+    # Print the address book
+    print(ab)
+
+    # Get a record by name
+    rec2 = ab["Jill"]
+    print(rec)
+
+    # Add a phone number to the record
+    rec2.add_phone(phone2)
+    print(rec2)
+    
+     # Change a phone number in the record
+    rec2.change_phone(Phone("56784"), Phone("99345"))
+
+    # Print the updated address book
+    print(ab)
